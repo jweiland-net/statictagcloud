2bc9a3456015f9ddf41983ad23eb8f250f673991
Stefan Frömken <froemken@gmail.com>
2022-11-30 14:09:55 +0100

    Update TYPO3 dependencies in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.17-10.4.99',
+            'typo3' => '10.4.32-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => []
